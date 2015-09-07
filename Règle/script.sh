7226b5504e67deea211532b63322b0ceb0e3a4e8
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2015-09-07 14:43:46 +0200

    Ajout d’un format A4 pour la notice

--- Règle/script.sh
+++ Règle/script.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 rst2html.py -l fr --link-stylesheet --initial-header-level 2 --stylesheet style.css regle.rst > regle.html
 weasyprint regle.html regle.pdf
+pdf2ps regle.pdf regle.ps
+psbook regle.ps regle_book.ps
+psnup -2 -w21cm -h25cm -W10.5cm -H25cm regle_book.ps regle_booklet.ps
+ps2pdf regle_booklet.ps regle_booklet.pdf
+rm *.ps
