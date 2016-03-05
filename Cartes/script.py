aa00b2e353dbad5de24b88bca1f630be07e59f64
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2016-03-05 17:56:39 +0100

    Correction du style des cartes

--- Cartes/script.py
+++ Cartes/script.py
@@ -22,11 +22,13 @@ Card = namedtuple('Card', [
 
 for back_type in ('missions', 'autres'):
     print('Rendu du dos %s' % back_type)
-    weasy = weasyprint.HTML('_dos_%s.html' % back_type)
+    filename = '_dos_%s.html' % back_type
+    weasy = weasyprint.HTML(filename)
     weasy.write_png(
-        os.path.join(u'Rendus', 'dos_%s.png' % back_type), resolution=30)
+        os.path.join('Rendus', 'dos_%s.png' % back_type), resolution=30)
 
 
+filenames = []
 for filename in os.listdir('.'):
     if not filename.startswith('_') and filename.endswith('.csv'):
         symbol, name = filename.split(' ', 1)
@@ -35,7 +37,7 @@ for filename in os.listdir('.'):
             reader = csv.reader(fd)
             next(reader)
             for i, line in enumerate(reader):
-                code = u'%s%02i' % (symbol, i + 1)
+                code = '%s%02i' % (symbol, i + 1)
                 card = Card(*line)
                 variables = dict(
                     (key, value if value != 'x' else '')
@@ -44,11 +46,19 @@ for filename in os.listdir('.'):
                 variables['code'] = code
                 variables['symbol'] = symbol
                 output_filename = (
-                    u'%s - %s' % (code, variables['title'])
+                    '%s - %s' % (code, variables['title'])
                     if variables['title'] else code)
-                output_file = os.path.join(u'Rendus', output_filename)
+                filenames.append(output_filename + '.html')
+                output_file = os.path.join('Rendus', output_filename)
                 print('Rendu de %s' % output_filename)
                 with open(output_file + '.html', 'w') as fd:
                     fd.write(html.render(**variables))
                 weasy = weasyprint.HTML(output_file + '.html')
                 weasy.write_png(output_file + '.png', resolution=30)
+
+
+cards_filename = os.path.join('Rendus', 'cartes.html')
+with open('_cartes.html') as fd:
+    cards_html = Template(fd.read())
+with open(cards_filename, 'w') as fd:
+    fd.write(cards_html.render(filenames=filenames))
